363c6d21d5566eff5a48ab778cfdbec4dd8fddc4
Dimitrios Souflis <dsouflis@acm.org>
2020-10-04 11:54:25 +0300

    0.0.9 dot-object recipes can be provided to fetch values from Redux for Swagger authorizations

--- src/swaggertoapi.js
+++ src/swaggertoapi.js
@@ -1,5 +1,6 @@
 const Handlebars = require("handlebars");
 const SwaggerClient = require('swagger-client');
+const dot = require('dot-object');
 
 const getClientForSwagger = url => new Promise((resolve, reject) => {
   const clientPromise = new SwaggerClient(url);
@@ -12,15 +13,50 @@ const getClientForSwagger = url => new Promise((resolve, reject) => {
 
 const clientTmpl = Handlebars.compile(`//{{name}} {{url}}
 import SwaggerClient from 'swagger-client';
+import { createSelectorCreator, defaultMemoize } from 'reselect';
+import { isEqual } from 'lodash';
+import dot from 'dot-object';
 
 const spec = {{{spec}}};
 
-const clientPromise = new SwaggerClient({
+const authorizationsStatic = {{{authorizationsStatic}}};
+const authorizations = {};
+
+const createDeepEqualSelector = createSelectorCreator(
+    defaultMemoize,
+    isEqual
+);
+
+const authorizationsRecipe = (state) => ({
+{{#each authorizationsDynamic}}
+  "{{key}}": {{{value}}},
+{{/each}}
+});
+
+const authorizationsSelector = createDeepEqualSelector(
+    state => {
+      let authorizationsDyn = authorizationsRecipe(state);
+      dot.object(authorizationsDyn);
+      console.log(authorizationsDyn);
+      return authorizationsDyn;
+    },
+    x => x
+);
+
+const updateAuthorizations = store => () => {
+  authorizations.authorizations = ({
+    ...authorizationsStatic,
+    ...authorizationsSelector(store.getState())
+  });
+};
+
+const clientPromise = (auth) => new SwaggerClient({
   spec,
+  authorizations: auth || authorizations.authorizations
 });
 
 const f = (section, method) => async (...args) => new Promise((resolve, reject) => {
-    clientPromise
+    clientPromise()
     .then(
         client => client.apis[section][method](...args),
         reason => reject(reason)
@@ -31,6 +67,10 @@ const f = (section, method) => async (...args) => new Promise((resolve, reject) 
   });
 
 const inst = {
+  _subscribeToReduxStore: (store) => {
+    store.subscribe(updateAuthorizations(store));
+    updateAuthorizations();
+  },
 {{#each operations}}
   {{operation}}: f('{{tag}}','{{operation}}'),
 {{/each}}  
@@ -43,7 +83,10 @@ export default inst;
       api= {
         name: 'PetStoreSwagger',
         url: 'https://petstore.swagger.io/v2/swagger.json',
-        //todo add auth specs.
+        authorizationsStatic: {},
+        authorizationsDynamic: {
+          api_key: 'authentication.access_token'
+        },
       }
 */
 const swaggerToApi = async (api) => {
@@ -51,10 +94,13 @@ const swaggerToApi = async (api) => {
   const operations = Object.entries(client.apis).flatMap(([tag, v]) => Object.keys(client.apis[tag]).map(operation => ({
     tag, operation
   })));
+  const recipe = Object.entries(api.authorizationsDynamic).map(entry => ({ key: entry[0], value: `dot.pick('${entry[1]}', state)`}));
   let s = clientTmpl({
     name: api.name,
     url: api.url,
     spec: JSON.stringify(client.spec, null, 2),
+    authorizationsStatic: JSON.stringify(api.authorizationsStatic, null, 2),
+    authorizationsDynamic: recipe,
     operations
   });
   return s;
